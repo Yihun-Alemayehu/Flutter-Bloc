21374bf6845ac8ac8fe34a5d97f5729c7cae6f9c
Yihun Alemayehu <yankure01@gmail.com>
2023-12-07 19:49:43 +0300

    fdb-009 commit

--- lib/example_2.dart
+++ lib/example_2.dart
@@ -18,20 +18,7 @@ class LoadPersonsAction implements LoadAction {
   }) : super();
 }
 
-@immutable
-class Person {
-  final String name;
-  final int age;
 
-  const Person({
-    required this.name,
-    required this.age,
-  });
-
-  Person.fromJson(Map<String, dynamic> json)
-      : name = json['name'] as String,
-        age = json['age'] as int;
-}
 
 enum PersonUrl {
   person1,
